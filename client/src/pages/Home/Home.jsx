0e4571f80d4861bf1e0bbef7d53cfc698edad44e
Aklile Ansa <aklilemente72@gmail.com>
2025-03-24 23:28:43 +0300

    added button in the link

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,13 +2,15 @@
   import Header from '../Header/Header'
   import Footer from '../Footer/Footer'
   import classes from './Home.module.css'
-  import { AppState } from '../../App'
+  import { AppState, QuestionState } from '../../App'
   import axios from '../../axiosBase'
   import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
   import {faUser,faGreaterThan} from '@fortawesome/free-solid-svg-icons'
-  import { Link } from 'react-router-dom'
+  import { Link, useNavigate } from 'react-router-dom'
 
   const Home = () => {
+    const navigate=useNavigate()
+    const {holdindex,setholdindex}=useContext(QuestionState)
       const {user}=useContext(AppState)
       const [userquestion,setuserquestion]=useState([])
       async function getallquestion() {
@@ -19,19 +21,23 @@
                     Authorization: 'Bearer '+ token
                   }
                 })
-                console.log(data.result[0].username)
+                // console.log(data.result[0].questionid)
                 setuserquestion(data.result)
                 // alert("questions found")
               } catch (error) {
                 console.log(error.response.data)
               }      
       }
+      const getkey=(keys)=>{
+        setholdindex(keys)
+        navigate(`/answerpage/${keys}`)
+      }
       useEffect(()=>{
         getallquestion()
       },[])
-      
-    return (
-      <>
+      console.log(holdindex)
+      return (
+        <>
       <Header/>
       <div className={classes.home}>
           <div className={classes.head}>
@@ -51,7 +57,9 @@
                   <FontAwesomeIcon icon={faUser} className={classes.user}/>  
                   <div className={classes.questionandbutton}>
                     <div className={classes.description}>{q.title}</div>
-                    <FontAwesomeIcon icon={faGreaterThan} className={classes.next}/>
+                   <button className={classes.next} onClick={()=>{getkey(q.questionid)}}>
+                  <Link>  <FontAwesomeIcon icon={faGreaterThan}  /></Link>
+                    </button> 
                   </div>
                   <div className={classes.username} >{q.username}</div>
                   <hr style={{marginLeft:"0px",border:"1px solid grey",marginRight:"0px",width:"965px",marginTop:"-6px",display:"block"}} />
